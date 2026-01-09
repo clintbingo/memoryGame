06c871974bd1be99e5bd8d0944f3a78d60ec8474
ian kane <iankane2003@yahoo.co.uk>
2026-01-09 16:46:02 +0000

    startGame function and endGame function added

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var images = [
-            'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?w=400',
-            'https://images.unsplash.com/photo-1548681528-6a5c45b66b42?w=400',
-            'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=400',
-            'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?w=400',
-            'https://images.unsplash.com/photo-1535268647677-300dbf3d78d1?w=400',
-            'https://images.unsplash.com/photo-1574158622682-e40e69881006?w=400',
-            'https://images.unsplash.com/photo-1560807707-8cc77767d783?w=400',
-            'https://images.unsplash.com/photo-1583337130417-3346a1be7dee?w=400'
-        ];
+    'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?w=400',
+    'https://images.unsplash.com/photo-1548681528-6a5c45b66b42?w=400',
+    'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=400',
+    'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?w=400',
+    'https://images.unsplash.com/photo-1535268647677-300dbf3d78d1?w=400',
+    'https://images.unsplash.com/photo-1574158622682-e40e69881006?w=400',
+    'https://images.unsplash.com/photo-1560807707-8cc77767d783?w=400',
+    'https://images.unsplash.com/photo-1583337130417-3346a1be7dee?w=400'
+];
 
 
 var firstCard = null;
@@ -17,21 +17,21 @@ var matches = 0;
 var moves = 0;
 var seconds = 0;
 var timeRunning = false;
-var timeInterval;
+var timerInterval;
 
 // function to start game
-function startGame(){
+function startGame() {
     var gameBoard = document.getElementById("gameBoard");
     gameBoard.innerHTML = "";
     //duplicates images in the array
     var cardImages = images.concat(images);
 
     //shuffle the cards
-    cardImages.sort(function(){
+    cardImages.sort(function () {
         return Math.random() - 0.5;
     })
 
-    for(var i = 0; i < cardImages.length; i++){
+    for (var i = 0; i < cardImages.length; i++) {
         var card = document.createElement('div');
         card.className = "card";
         card.innerHtml = `<div class="card-front"><i class="fas fa-heart"></i></div>
@@ -47,53 +47,53 @@ function startGame(){
     moves = 0;
     seconds = 0;
     timeRunning = false;
-    
+
 
     updateStats();
     clearInterval(timeInterval)
 }
 
-function flipCard(){
+function flipCard() {
     if (!canFlip) return
 
-    if(this.classList.contains("flipped")) return
-    if(this.classList.contains("matched")) return
-    
-    if(!timerRunning){
+    if (this.classList.contains("flipped")) return
+    if (this.classList.contains("matched")) return
+
+    if (!timerRunning) {
         startTimer()
     }
 
     this.classList.add("flipped")
 
-    if(firstCard == null){
+    if (firstCard == null) {
         firstCard = this;
-    } else{
+    } else {
         secondCard = this;
-        canFlip =false;
+        canFlip = false;
         moves++
         updateStats()
         checkMatch()
     }
 }
 
-function checkMatch(){
+function checkMatch() {
     var match = firstCard.dataset.image == secondCard.dataset.image;
 
-    if(match){
-        setTimeout(()=>{
+    if (match) {
+        setTimeout(() => {
             firstCard.classList.add("matched")
             secondCard.classList.add("matched")
             matches++;
             updateStats()
             resetCards()
 
-            if(matches = 8){
+            if (matches = 8) {
                 endGame()
             }
 
         }, 500);
-    }else{
-        setTimeout(()=>{
+    } else {
+        setTimeout(() => {
             firstCard.classList.remove("flipped")
             secondCard.classList.remove("flipped")
             resetCards()
@@ -103,13 +103,13 @@ function checkMatch(){
 
 
 
-function resetCard(){
+function resetCard() {
     firstCard = null
     secondCard = null
-    canFlip= true
+    canFlip = true
 }
 
-function startTimer(){
+function startTimer() {
     timerRunning = true
     timerInterval = setInterval(() => {
         seconds++
@@ -117,17 +117,29 @@ function startTimer(){
     }, 1000);
 }
 
-function updateStats(){
+function updateStats() {
     document.getElementById('moves').textContent = moves
     document.getElementById('matches').textContent = matches + "/8"
 
     var mins = Math.floor(seconds / 60)
     var secs = seconds % 60
-    if(secs < 10) secs = "0" + secs
+    if (secs < 10) secs = "0" + secs
     document.getElementById('time').textContent = mins + ":" + secs
 
 }
 
+function endGame() {
+    clearInterval(timerInterval)
+    document.getElementById("firstMoves").textContent = moves;
+    document.getElementById("finalTime").textContent = document.getElementById('time').textContent;
+    document.getElementById("winModal").classList.add("show");
+}
+
+function newGame(){
+    document.getElementById('winModal').classList.remove('show');
+    clearInterval(timerInterval)
+    startGame();
+}
 
 startGame()
 
